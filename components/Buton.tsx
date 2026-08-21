cc9d19af8fc22fbb8339d29632aae92964002d6f
cirjeudumitru-prog <cirjeu.dumitru@elev.cihcahul.md>
2026-08-21 09:20:50 +0300

    buton mai responsiv textul

--- components/Buton.tsx
+++ components/Buton.tsx
@@ -14,7 +14,7 @@ const BlueButon = (props: any) => {
             whileHover={{ scale: 1.05, }}
             whileTap={{ scale: 0.9, }}
             transition={{ type: "spring", damping: 7, stiffness: 300 }}
-            className="butoaneMobile inline-block bg-[#00043B] hover:bg-[#0c005e] text-[10px] sm:text-[14px] md:text-[16px] text-white font-serif px-3 sm:px-4 md:px-5 lg:px-6 py-1 rounded border border-blue-400/30 transition-colors"
+            className="butoaneMobile inline-block bg-[#00043B] hover:bg-[#0c005e] text-[12px] sm:text-[14px] md:text-[16px] text-white font-serif px-4 sm:px-5 md:px-6 lg:px-7 py-1 rounded border border-blue-400/30 transition-colors"
         >
             {props.continut}
         </MotionLink>
@@ -27,7 +27,7 @@ const WhiteButon = (props: any) => {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.9 }}
             transition={{ type: 'spring', damping: 7, stiffness: 300 }}
-            className="butoaneMobile inline-block bg-[#ebebeb] hover:bg-[#d7d7d7] text-[10px] sm:text-[14px] md:text-[16px] text-black font-serif px-3 sm:px-4 md:px-5 lg:px-6 py-1 rounded border border-gray-600 transition-colors"        >
+            className="butoaneMobile inline-block bg-[#ebebeb] hover:bg-[#d7d7d7] text-[12px] sm:text-[14px] md:text-[16px] text-black font-serif px-4 sm:px-5 md:px-6 lg:px-7 py-1 rounded border border-gray-600 transition-colors"        >
             {props.continut}
         </MotionLink>
     );
